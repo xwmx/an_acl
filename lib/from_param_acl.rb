9b3a464bcdf89cc6dd987409e65d40d24125c96c
William Melody <wmelody@gmail.com>
2008-10-06 17:51:43 -0500

    removing array and compact from arguments in permitted?

--- lib/from_param_acl.rb
+++ lib/from_param_acl.rb
@@ -28,13 +28,13 @@ module FromParamAcl
       
       # Override in model to define permissions for model reads.
       # Defaults to true, providing access to any user.
-      def is_readable_by?(agent, current_context = nil)
+      def is_readable_by?(agent = nil, current_context = nil)
         true
       end
       
       # Override in model to define permissions for new object creation.
       # Defaults to true, providing access to any user.
-      def is_creatable_by?(agent, current_context = nil)
+      def is_creatable_by?(agent = nil, current_context = nil)
         true
       end
     
@@ -42,19 +42,19 @@ module FromParamAcl
       
     # Override in model to define permissions for reading instances.
     # Defaults to true, providing access to any user.
-    def is_readable_by?(agent)
+    def is_readable_by?(agent = nil)
       true
     end
     
     # Override in model to define permissions for updating instances.
     # Defaults to true, providing access to any user.
-    def is_updatable_by?(agent)
+    def is_updatable_by?(agent = nil)
       true
     end
     
     # Override in model to define permissions for deleting instances.
     # Defaults to ActiveRecords::Base#is_updatable_by?(agent)
-    def is_deletable_by?(agent)
+    def is_deletable_by?(agent = nil)
       is_updatable_by?(agent)
     end
   end
@@ -102,11 +102,11 @@ module FromParamAcl
 
         case params[:action]
         when "index"
-          klass.is_readable_by?(*[current_user, @context].compact)
+          klass.is_readable_by?(current_user, @context)
         when "show"
           object && object.is_readable_by?(current_user)
         when "new", "create"
-          klass.is_creatable_by?(*[current_user, @context].compact)
+          klass.is_creatable_by?(current_user, @context)
         when "edit", "update"
           object && object.is_updatable_by?(current_user)
         when "destroy"
