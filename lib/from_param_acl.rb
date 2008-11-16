100737c13d13b322f62b4529cc0ae90ceae74a98
William Melody <wmelody@gmail.com>
2008-11-15 20:11:21 -0600

    adding a current_context method to provide another way to set the context

--- lib/from_param_acl.rb
+++ lib/from_param_acl.rb
@@ -108,25 +108,32 @@ module FromParamAcl
     protected
 
       # Override in controller to have custom permissions for the resource.
-      # If permissions are dependent on context, set the @context variable.
-      # On object actions, expects the existence of instance variable using
-      # the singular name of the current resource.
+      # If permissions are dependent on context, override the current_context
+      # method or assign a value to an @context variable. On object actions, 
+      # expects the existence of instance variable using the singular name of 
+      # the current resource.
       def permitted?
         klass = self.controller_name.classify.constantize
         object = instance_variable_get("@#{self.controller_name.singularize}")
-
+      
         case params[:action]
         when "index"
-          klass.is_readable_by?(current_user, @context)
+          klass.is_readable_by?(current_user, current_context)
         when "show"
           object && object.is_readable_by?(current_user)
         when "new", "create"
-          klass.is_creatable_by?(current_user, @context)
+          klass.is_creatable_by?(current_user, current_context)
         when "edit", "update"
           object && object.is_updatable_by?(current_user)
         when "destroy"
           object && object.is_deletable_by?(current_user)
         end
+      end
+      
+      # Override in controller or assign an object to an @context variable 
+      # to provide a context for class-level permissions.
+      def current_context
+        @context
       end
   end
 end
