81656657425a743b4bb313f8e664b6920cfa40dd
William Melody <wmelody@gmail.com>
2009-11-19 14:51:25 -0600

    removing unnecessary File.dirname from requires

--- lib/from_param_acl.rb
+++ lib/from_param_acl.rb
@@ -1,6 +1,6 @@
 # FromParamAcl
-require File.dirname(__FILE__) + '/from_param_acl/model_permissions'
-require File.dirname(__FILE__) + '/from_param_acl/controller_permissions'
-require File.dirname(__FILE__) + '/from_param_acl/finder_filters'
-require File.dirname(__FILE__) + '/from_param_acl/from_param'
+require 'from_param_acl/model_permissions'
+require 'from_param_acl/controller_permissions'
+require 'from_param_acl/finder_filters'
+require 'from_param_acl/from_param'
 
