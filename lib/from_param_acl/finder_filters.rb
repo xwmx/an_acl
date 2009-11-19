44e4b0dd6e3580a15f836605aa1c18f949418ad0
William Melody <wmelody@gmail.com>
2009-11-19 15:15:26 -0600

    moving finder filter object name and class to methods

--- lib/from_param_acl/finder_filters.rb
+++ lib/from_param_acl/finder_filters.rb
@@ -4,13 +4,23 @@ module FromParamAcl
       # Use as a before filter to get object for the resource if current_user
       # has the necessary permissions.
       def get_from_param_for
-        instance_variable_set("@#{self.controller_name.singularize}", self.controller_name.classify.constantize.from_param_for(current_user, action_name, params[:id]))
+        instance_variable_set("@#{obj_name}", obj_class.from_param_for(current_user, action_name, params[:id]))
       end
 
       # Use as a before filter to get object for the resource without checking
       # permissions.
       def get_from_param
-        instance_variable_set("@#{self.controller_name.singularize}", self.controller_name.classify.constantize.from_param(params[:id]))
+        instance_variable_set("@#{obj_name}", obj_class.from_param(params[:id]))
+      end
+    
+    protected
+      
+      def obj_name
+        self.controller_name.singularize
+      end
+      
+      def obj_class
+        self.controller_name.classify.constantize
       end
     end
   end
